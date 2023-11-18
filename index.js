4125bb4f5680a80390964b91521d7e9ecf6b4b56
RajRagavi <ragavirajasekaran2590@gmail.com>
2023-11-18 16:37:48 +0530

    new file

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ app.use(bodyParser.json());
 
 app.use('/rooms', roomRoutes);
 app.use('/bookings', bookingRoutes);
+app.get('/', (req, res) => {
+    // Example local variable
+    const message = '<h1>Hello, World!</h1>';
 
+    // Send the message as a response
+    res.send(message);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
